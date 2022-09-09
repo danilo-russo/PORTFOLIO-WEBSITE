79774461a049c7bc5b5f1f6339bc8f489e194cb1
MR Chao <danilorusso995@gmail.com>
2022-09-09 17:20:40 +0200

    Aggiunto tempo che scorre

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ let hiddenElement3 = document.getElementsByClassName("hidden")[2];
 let textbutton1 = document.getElementsByClassName("namePro")[0];
 let textbutton2 = document.getElementsByClassName("namePro")[1];
 let textbutton3 = document.getElementsByClassName("namePro")[2];
+let a = document.getElementById("a");
+let b = document.getElementById("b");
+let c = document.getElementById("c");
+let d = document.getElementById("d");
+let aTime = 0;
+let bTime = 0;
+let cTime = 0;
+let dTime = 0;
 
 function hidden1() {
   if (hiddenElement1.style.display === "block") {
@@ -32,3 +40,26 @@ function hidden3() {
 textbutton1.addEventListener("click", hidden1);
 textbutton2.addEventListener("click", hidden2);
 textbutton3.addEventListener("click", hidden3);
+
+setInterval(() => {
+  aTime++;
+  a.innerHTML = aTime;
+  if (aTime === 999) {
+    aTime = 0;
+    a.innerHTML = aTime;
+    bTime++;
+    b.innerHTML = bTime;
+    if (bTime === 999) {
+      bTime = 0;
+      b.innerHTML = bTime;
+      cTime++;
+      c.innerHTML = cTime;
+    }
+    if (cTime === 999) {
+      cTime = 0;
+      c.innerHTML = cTime;
+      dTime++;
+      d.innerHTML = dTime;
+    }
+  }
+}, 1000);
